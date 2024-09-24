e51e467270d11d6f759a992c7c8f47f673ed1faf
Kayathri-maths <kayualagarsamy@gmail.com>
2024-09-24 18:46:51 +0530

    CRUD Operations

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import { useState, useEffect } from "react";
 import SearchItem from "./SearchItem";
+import apiRequest from "./apiRequest";
 function App() {
   const API_URL = "http://localhost:3500/items";
 
@@ -35,26 +36,51 @@ function App() {
     }, 2000);
   }, []);
 
-  const addItem = (item) => {
-    const id = items.length ? items[items.length - 1].id + 1 : 1;
+  const addItem = async (item) => {
+    const id = items.length ? parseInt(items[items.length - 1].id) + 1 : 1;
     const addNewItem = { id, checked: false, item };
     const listItems = [...items, addNewItem];
     setItems(listItems);
-    localStorage.setItem("todo_list", JSON.stringify(listItems));
+
+    const postOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(addNewItem),
+    };
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleCheck = (id) => {
+  const handleCheck = async (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
     setItems(listItems);
-    localStorage.setItem("todo_list", JSON.stringify(listItems));
+    const myItem = listItems.filter((item) => item.id === id);
+    console.log(myItem);
+    const updateOptions = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ checked: myItem[0].checked }),
+    };
+    const reqURL = `${API_URL}/${id}`;
+    const result = await apiRequest(reqURL, updateOptions);
+    if (result) setFetchError(result);
   };
 
-  const handleDelete = (id) => {
+  const handleDelete = async (id) => {
     const listItems = items.filter((item) => item.id !== id);
     setItems(listItems);
-    localStorage.setItem("todo_list", JSON.stringify(listItems));
+    const deleteOptions = {
+      method: "DELETE",
+    };
+    const reqURL = `${API_URL}/${id}`;
+    const result = await apiRequest(reqURL, deleteOptions);
+    if (result) setFetchError(result);
   };
 
   const handleSubmit = (e) => {
